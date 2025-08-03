60e6913d7f18d7c6177ad025f15b9efe510d35a6
mohamed <mohamedabdelbakyali@gmail.com>
2025-08-03 22:27:28 +0300

    bank task

---
+++ C#/bank task/bank task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        BankAccount account1 = new BankAccount(
+            "Alice Smith",
+            "30310013412698",
+            "01012345678",
+            "1428 Elm Street",
+            1500.75m
+        );
+
+        BankAccount account2 = new BankAccount(
+            "Bob Johnson",
+            "30310069413658",
+            "01123456789",
+            "360 Qalyub Elbalad"
+        );
+        BankAccount account3 = new BankAccount();
+
+        // Display details
+        account1.ShowAccountDetails();
+        account2.ShowAccountDetails();
+        Console.WriteLine("---- DefaultAccount Details ----");
+        account3.ShowAccountDetails();
+    }
+}
---
+++ C#/bank task/bank task/bank.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+
+public class BankAccount
+{
+    public const string BankCode = "BNK001";
+    public readonly DateTime createdDate;
+    private static int allacountsnumber = 1;
+    private int accountNumber;
+    private string fullName;
+    private string nationalID;
+    private string phoneNumber;
+    private string address;
+    private decimal balance;
+    public int AccountNumber => accountNumber;
+    public DateTime CreatedDate { get; }
+
+    public string FullName
+    {
+        get { return fullName; }
+        private set {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Full name cannot be null or empty.");
+            }
+            fullName = value;
+        } 
+    }
+     public decimal Balance  {
+         get { return balance; }
+        private set {
+            if (value < 0) { 
+            throw new ArgumentException("you can'ot enter negative ballance");
+            }
+            balance = value;
+
+        }
+    }
+    public string Address
+    {
+        get { return address; }
+        set { address = value; }
+    }
+    public string PhoneNumber{  get { return phoneNumber; }
+        private set {
+         if (IsValidPhoneNumber(value))
+            {
+                phoneNumber = value;
+
+            }
+            else
+            {
+                throw new ArgumentException("the phone number must be 11 digits and start with 01");
+            }
+        } 
+    }
+     public string NationalID{  get { return nationalID; }
+        private set {
+            if (IsValidNationalID(value))
+            {
+                nationalID = value;
+            }
+            else {
+                throw new ArgumentException("the id should be 14 digits");
+            }
+
+        } 
+    }
+ 
+
+
+    public BankAccount()
+    {
+        accountNumber= allacountsnumber++;
+        FullName = "John Doe";
+        NationalID = "00000000000000";
+        PhoneNumber = "01000000000";
+        Address = "Default Address";
+        Balance = 0;
+        CreatedDate = DateTime.Now;
+    }
+
+    public BankAccount(string fullName, string nationalID, string phoneNumber, string address, decimal balance)
+    {
+        accountNumber= allacountsnumber++;
+        FullName = fullName;
+        NationalID = nationalID;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        Balance = balance;
+        CreatedDate = DateTime.Now;
+    }
+
+    public BankAccount(string fullName, string nationalID, string phoneNumber, string address)
+        : this(fullName, nationalID, phoneNumber, address, 0) { }
+
+    public void ShowAccountDetails()
+    {
+        Console.WriteLine("---- Account Details ----");
+        Console.WriteLine($"Bank Code: {BankCode}");
+        Console.WriteLine($"Account Number: {accountNumber}");
+        Console.WriteLine($"Full Name: {FullName}");
+        Console.WriteLine($"National ID: {NationalID}");
+        Console.WriteLine($"Phone Number: {PhoneNumber}");
+        Console.WriteLine($"Address: {Address}");
+        Console.WriteLine($"Balance: {Balance:c}");
+        Console.WriteLine($"Created Date: {CreatedDate}");
+        Console.WriteLine("-------------------------\n");
+    }
+    public bool IsValidNationalID(string nationalID)
+    {
+        return nationalID != null
+            && nationalID.Length == 14
+            && nationalID.All(char.IsDigit);
+    }
+
+    public bool IsValidPhoneNumber(string phoneNumber)
+    {
+        return phoneNumber != null
+            && phoneNumber.Length == 11
+            && phoneNumber.StartsWith("01");
+    }
+
+
+
+}
